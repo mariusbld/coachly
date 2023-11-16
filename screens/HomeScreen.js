4432ec027319dce27ed9823aaf28e178d83a42cd
Marius Buleandra <mbuleandra@gmail.com>
2023-11-16 14:07:32 -0800

    done

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, TouchableOpacity } from 'react-native';
 
 export default function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text style={styles.textLogo}>[Coachly Logo]</Text>
-      <Button title='Voice Coach' onPress={() => navigation.navigate('Voice Coach')} />
-      <Button title='View Progress' onPress={() => navigation.navigate('Progress')} />
+        <Image
+          source={require('../assets/coachly-logo.jpeg')}
+          style={{ width: 320, height: 320 }} // Adjust the size as needed
+        />
+      {/* <Button style={styles.actionButton} title='Voice Coach' onPress={() => navigation.navigate('Voice Coach')} />
+      <Button title='View Progress' onPress={() => navigation.navigate('Progress')} /> */}
+      <TouchableOpacity 
+        style={styles.button} 
+        onPress={() => navigation.navigate('Voice Coach')}
+      >
+        <Text style={styles.buttonText}>Voice Coach</Text>
+      </TouchableOpacity>
+      <TouchableOpacity 
+        style={styles.button} 
+        onPress={() => navigation.navigate('Progress')}
+      >
+        <Text style={styles.buttonText}>View Progress</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -23,5 +38,22 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 20,
-  }
+  },
+  actionButton: {
+    color: '#ff0000',
+    fontSize: 24,
+  },
+  button: {
+    margin: 4,
+    backgroundColor: '#028a87',
+    padding: 12,
+    borderRadius: 5,
+    width: 180,
+    textAlign: 'center',
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: 'white',
+    fontSize: 24,
+  },
 });
--- screens/LessonScreen.js
+++ screens/LessonScreen.js
@@ -1,11 +1,12 @@
 import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TouchableOpacity, Image } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity, Image, TextInput } from 'react-native';
 import { Audio } from 'expo-av';
 
 export default function LessonScreen() {
   const [recording, setRecording] = React.useState();
   const [uri, setUri] = React.useState();
+  const [stats, setStats] = React.useState({ text: "Hello, just testing this", wpm: 100});
 
   async function startRecording() {
     try {
@@ -15,8 +16,6 @@ export default function LessonScreen() {
         allowsRecordingIOS: true,
         playsInSilentModeIOS: true,
       });
-
-      console.log('Starting recording..');
       const { recording } = await Audio.Recording.createAsync( Audio.RecordingOptionsPresets.HIGH_QUALITY
       );
       setRecording(recording);
@@ -38,18 +37,21 @@ export default function LessonScreen() {
     const uri = recording.getURI();
     setUri(uri);
     console.log('Recording stopped and stored at', uri);
+    await sendFile();
   }
 
   async function sendFile() {
     var body = new FormData();
-    body.append('inputFile', {
+    body.append('file', {
        name: 'sound.mp4',
        type: 'audio/mp3',
        uri: uri
     });
+    const API_URL = 'https://0b55-67-207-107-182.ngrok.io/upload';
+    const API_URL_LIVE = 'https://1dec-67-207-107-182.ngrok-free.app/transcribe';
     // console.log("+++++++=========body=========++++++",body);
     try {
-        const response = await fetch('https://0b55-67-207-107-182.ngrok.io/upload', {
+        const response = await fetch(API_URL_LIVE, {
           method: 'POST',
           body: body,
           headers: {
@@ -60,6 +62,8 @@ export default function LessonScreen() {
         });
         const responseJson = await response.json();
         console.log("+++++++=========responseJson=========++++++",responseJson);
+        const { text, wpm } = responseJson;
+        setStats({ text, wpm });
       } catch (error) {
         console.log(error);
       }
@@ -74,25 +78,41 @@ export default function LessonScreen() {
   //   await sound.playAsync();
   // }
 
+  async function reset() {
+    setStats(undefined);
+  }
+
+  if (stats) {
+    return (
+      <View style={styles.container}>
+        {/* <Text style={styles.textLogo}>Your Speech</Text> */}
+        <Text style={styles.textLogo}>WPM: {stats.wpm}</Text>
+        <TextInput style={styles.textSpeech}>{stats.text}</TextInput>
+        <TouchableOpacity 
+          style={styles.button} 
+          onPress={reset}
+        >
+          <Text style={styles.buttonText}>Try Again</Text>
+        </TouchableOpacity>
+          <StatusBar style="auto" />
+      </View>
+    )
+  }
+
   return (
     <View style={styles.container}>
-      {!!recording && <TouchableOpacity onPress={startRecording}>
+      {!!recording && <TouchableOpacity onPress={stopRecording}>
         <Image
           source={require('../assets/stop-icon.jpeg')}
-          style={{ width: 120, height: 120 }} // Adjust the size as needed
+          style={{ width: 100, height: 100 }}
         />
       </TouchableOpacity>}
       {!recording && <TouchableOpacity onPress={startRecording}>
         <Image
           source={require('../assets/mic-icon.png')}
-          style={{ width: 120, height: 120 }} // Adjust the size as needed
+          style={{ width: 100, height: 100 }}
         />
       </TouchableOpacity>}
-      <Button
-        title={recording ? 'Stop Recording' : 'Start Recording'}
-        onPress={recording ? stopRecording : startRecording}
-      />
-      <Text>{recording ? "recording" : "stopped"}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -107,7 +127,27 @@ const styles = StyleSheet.create({
   },
   textLogo: {
     fontSize: 24,
-    fontWeight: 'bold',
     marginBottom: 20,
-  }
+  },
+  textSpeech: {
+    backgroundColor: '#C2FEF3',
+    fontSize: 18,
+    padding: 12,
+    borderRadius: 8,
+    marginBottom: 20,
+    minWidth: 300,
+  },
+  button: {
+    margin: 4,
+    backgroundColor: '#028a87',
+    padding: 12,
+    borderRadius: 5,
+    width: 180,
+    textAlign: 'center',
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: 'white',
+    fontSize: 24,
+  },
 });
